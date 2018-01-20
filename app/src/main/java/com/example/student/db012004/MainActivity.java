67fc15cf83a8b70666eeef349f8a9e57714a8c84
Shaojung <shaojung@gmail.com>
2018-01-20 15:40:37 +0800

    get SeekBar progress

--- app/src/main/java/com/example/student/db012004/MainActivity.java
+++ app/src/main/java/com/example/student/db012004/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.student.db012004;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    SeekBar sb;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sb = (SeekBar) findViewById(R.id.seekBar);
+        tv = (TextView) findViewById(R.id.textView);
+    }
+    public void click1(View v)
+    {
+        int p = sb.getProgress();
+        tv.setText(String.valueOf(p));
     }
 }
